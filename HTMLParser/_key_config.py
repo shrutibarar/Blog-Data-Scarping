80287bfe9d415ff57120789d9084a8fe68a699eb
shruti <shrutibarar08@gmail.com>
2023-09-12 00:00:03 +0530

    restructure _key_config

--- HTMLParser/_key_config.py
+++ HTMLParser/_key_config.py
@@ -4,10 +4,32 @@ base_key_format = {
 
     # main content to be scrapped from focus
     "focus": {
-        "div": "ch bg et eu ev ew"
+        "div": "class_name"
     },
 
     # tags and their respective titles and contents to be scrapped
+    "contents": {
+        "h1": {
+            "class_name": "Title",
+        },
+        "h2": {
+            "class_name": "subtitle",
+        },
+    },
+    "figures": {
+        "path": None  # if none then use a default path
+    },
+    "links": False  # either include links or not by default its false
+}
+
+
+# medium website content format keys
+medium_key_format = {
+
+    "focus": {
+        "div": "ch bg et eu ev ew"
+    },
+
     "contents": {
         "h1": {
             "pw-post-title": "Title",
@@ -21,8 +43,6 @@ base_key_format = {
         "li": {
             "data-selectable-paragraph": None
         },
-    },
-    "paragraph": {
         "p": {
             " pw-post-body-paragraph": None,
             "em": None,
@@ -32,53 +52,7 @@ base_key_format = {
         },
     },
     "figures": {
-        "path": None    # if none then use a default path
+        "path": None
     },
-    "links": False      # either include links or not by default its false
- }
-
-
-Medium_format = {
-
-     "focus": {
-        "div": "ch bg et eu ev ew"
-     },
-
-    # tags and their respective titles and contents to be scrapped
-    "contents": {
-        "h1": {
-            "pw-post-title": "Title",
-            "Data-selectable-paragraph": "Key points"
-        },
-        "h2": {
-            "Pw-subtitle-paragraph": "subtitle",
-            "sub-sub-title": "major points",
-        },
-
-        "li": {
-            "data-selectable-paragraph": None
-        },
-     },
-
-     "paragraph": {
-        "p": {
-            " pw-post-body-paragraph": None,
-            "em": None,
-            "a": None,
-            "af nc": None,
-            "data-selectable-paragraph": None
-        },
-     },
-     "figures": {
-        "path": None    # if none then use a default path
-     },
-    "links": False      # either include links or not by default its false
-
-}
-
-
-# keys for Medium website
-
-medium_key_format = {
-
+    "links": False
 }
