a1def0e2854cb65286bb4fbd3eebea47f9cf4a9f
shruti barar <shrutibarar08@gmail.com>
2023-09-12 01:24:17 +0530

    extract class

---
+++ HTMLParser/_extractors.py
@@ -0,0 +1,16 @@
+class Extractor:
+    def __init__(self, storage):
+        self.storage = storage
+
+    def _extract_keys(self):
+        pass
+
+    def _iterate_page(self, storage):
+        pass
+
+    def remove_unwanted(self, page):
+        pass
+
+    def add_content(self, content):
+        pass
+
