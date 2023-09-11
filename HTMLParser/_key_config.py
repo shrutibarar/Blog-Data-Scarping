946f3cf465c7c4ac7e93915f54a86698f7210fe2
shruti barar <shrutibarar08@gmail.com>
2023-09-11 23:47:02 +0530

    medium formate

--- HTMLParser/_key_config.py
+++ HTMLParser/_key_config.py
@@ -38,6 +38,43 @@ base_key_format = {
  }
 
 
+Medium_format = {
+
+     "focus": {
+        "div": "ch bg et eu ev ew"
+     },
+
+    # tags and their respective titles and contents to be scrapped
+    "contents": {
+        "h1": {
+            "pw-post-title": "Title",
+            "Data-selectable-paragraph": "Key points"
+        },
+        "h2": {
+            "Pw-subtitle-paragraph": "subtitle",
+            "sub-sub-title": "major points",
+        },
+
+        "li": {
+            "data-selectable-paragraph": None
+        },
+     },
+
+     "paragraph": {
+        "p": {
+            " pw-post-body-paragraph": None,
+            "em": None,
+            "a": None,
+            "af nc": None,
+            "data-selectable-paragraph": None
+        },
+     },
+     "figures": {
+        "path": None    # if none then use a default path
+     },
+    "links": False      # either include links or not by default its false
+
+}
 
 
 # keys for Medium website
